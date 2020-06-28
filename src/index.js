f299b9751b647fe02889028f2dac6711e5ca2f5a
Erling Tokheim <e@tokheim.no>
2020-06-28 14:52:40 +0200

    Feat: Add Redux Provider to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './components/App/App'
 import * as serviceWorker from './serviceWorker'
+import reducer from './reducers'
+import middleware from './middleware'
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
