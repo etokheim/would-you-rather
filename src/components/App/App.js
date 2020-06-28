0cd44ac64fcaddddd386a1953b7a4fed8d177d4b
Erling Tokheim <e@tokheim.no>
2020-06-28 19:19:51 +0200

    Fix: Only render login component if not logged in
    Rendering components that needs userdata will cause errors.
    
    Note that the redirect doesn't trigger before the whole page is rendered

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,11 @@ import Login from '../Login/Login'
 import Home from '../Home/Home'
 import handleInitialData from './../../actions/shared'
 
+function mapStateToProps({ authenticatedUser }) {
+	return {
+		authenticatedUser
+	}
+}
 
 export default connect(mapStateToProps)(class App extends Component {
 	static propTypes = {
@@ -23,24 +28,21 @@ export default connect(mapStateToProps)(class App extends Component {
 		return (
 			<BrowserRouter>
 				{
-					// Redirect to login page if the user hasn't logged in
-					!authenticatedUser ? <Redirect to="/login" /> : ""
+					// Redirect to login page if the user hasn't logged in, else we can render stuff
+					// that need user data.
+					!authenticatedUser ? <Redirect to="/login" /> : (
+						<Switch>
+							<Route exact path='/'>
+								<Home />
+							</Route>
+						</Switch>
+					)
 				}
-				<Switch>
-					<Route exact path='/'>
-						<Home />
-					</Route>
-					<Route path='/login'>
-						<Login />
-					</Route>
-				</Switch>
+
+				<Route path='/login'>
+					<Login />
+				</Route>
 			</BrowserRouter>
 		)
 	}
 })
-
-function mapStateToProps({ authenticatedUser }) {
-	return {
-		authenticatedUser
-	}
-}
