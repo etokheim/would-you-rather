82d70064b739606fd4cd252f5b96d8b0f161ae46
Erling Tokheim <e@tokheim.no>
2020-06-29 16:28:00 +0200

    Feat: Highlight answered question

--- src/components/WouldYouRather/WouldYouRather.js
+++ src/components/WouldYouRather/WouldYouRather.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import AppBar from '../AppBar/AppBar'
 import { connect } from 'react-redux'
 import { handleAnswerQuestion } from '../../actions/questions'
+import authenticatedUser from '../../reducers/authenticatedUser'
 
 function mapStateToProps(props) {
 	return {
@@ -24,19 +25,31 @@ export default connect(mapStateToProps)(class WouldYouRather extends Component {
 	}
 
 	render() {
-		const { match: { params }, questions, users } = this.props;
+		const { match: { params }, questions, users, authenticatedUser } = this.props;
 		const question = questions[params.questionId]
 		const author = users[question.author]
+
+		// If optionOne contains user, she answered question 0
+		// else if optionTwo contains user, she answered question 1
+		const answeredQuestion = question.optionOne.votes.find((vote) => vote === authenticatedUser)
+								? 0
+								: question.optionTwo.votes.find((vote) => vote === authenticatedUser)
+									? 1
+									: undefined
 
 		return (
 			<div>
 				Would you rather
 				<div className="wouldYouRather">
 					<div className="option" onClick={ () => this.handleChoose(0)}>
-						{ question.optionOne.text }
+						<span style={{ color: answeredQuestion === 0 ? "green" : "" }}>
+							{ question.optionOne.text }
+						</span>
 					</div>
 					<div className="option" onClick={ () => this.handleChoose(1)}>
-						{ question.optionTwo.text }
+						<span style={{ color: answeredQuestion === 1 ? "green" : "" }}>
+							{ question.optionTwo.text }
+						</span>
 					</div>
 					<div className="author">
 						<div className="picture">
