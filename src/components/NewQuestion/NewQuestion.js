5ad5831e772f0d4dc68d882622f23f910d24ade4
Erling Tokheim <e@tokheim.no>
2020-07-01 11:39:07 +0200

    Feat: Ui for asking questions

--- src/components/NewQuestion/NewQuestion.js
+++ src/components/NewQuestion/NewQuestion.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import AppBar from '../AppBar/AppBar'
+import { connect } from 'react-redux'
 
-export default class NewQuestion extends Component {
+const mapStateToProps = ({ }) => { return { } }
+
+export default connect(mapStateToProps)(class NewQuestion extends Component {
 	static propTypes = {
 		prop: PropTypes
+	}
+
+	state = {
+		optionOneText: "",
+		optionTwoText: ""
+	}
+
+	handleInput = (event, stateProperty) => {
+		const value = event.target.value
+
+		this.setState({
+			[stateProperty]: value
+		})
+	}
+
+	handleAskQuestion = (event) => {
+		event.preventDefault()
+		console.log("Clicked button");
+
+		// this.props.dispatch()
 	}
 
 	render() {
 		return (
 			<div>
-				New Question
+				<h1>New Question</h1>
+				<textarea onChange={ (event) => this.handleInput(event, "optionOneText") } />
+				<textarea onChange={ (event) => this.handleInput(event, "optionTwoText") } />
+				<button type="submit" onClick={ this.handleAskQuestion }>Ask question</button>
 				<AppBar />
 			</div>
 		)
 	}
-}
+})
