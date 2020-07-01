591645cf97d55a4c67e8b7867d8006fdf19f5cfc
Erling Tokheim <e@tokheim.no>
2020-07-01 14:55:11 +0200

    Feat: Show answer statistics on answered questions

--- src/components/WouldYouRather/WouldYouRather.js
+++ src/components/WouldYouRather/WouldYouRather.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import AppBar from '../AppBar/AppBar'
 import { connect } from 'react-redux'
 import { handleAnswerQuestion } from '../../actions/questions'
+import './wouldYouRather.scss'
 
 function mapStateToProps(props) {
 	return {
@@ -37,10 +38,10 @@ export default connect(mapStateToProps)(class WouldYouRather extends Component {
 		// If optionOne contains user, she answered optionOne
 		// else if optionTwo contains user, she answered optionTwo
 		const answeredQuestion = question.optionOne.votes.find((vote) => vote === authenticatedUser)
-								? "optionOne"
-								: question.optionTwo.votes.find((vote) => vote === authenticatedUser)
-									? "optionTwo"
-									: undefined
+			? "optionOne"
+			: question.optionTwo.votes.find((vote) => vote === authenticatedUser)
+				? "optionTwo"
+				: undefined
 
 		this.setState({
 			answeredQuestion
@@ -48,24 +49,37 @@ export default connect(mapStateToProps)(class WouldYouRather extends Component {
 	}
 
 	render() {
-		const { match: { params }, questions, users, authenticatedUser } = this.props;
+		const { match: { params }, questions, users } = this.props;
 		const question = questions[params.questionId]
 		const author = users[question.author]
 		const { answeredQuestion } = this.state
+		const optionOneVotes = question.optionOne.votes.length
+		const optionTwoVotes = question.optionTwo.votes.length
+		const totalVotes = optionOneVotes + optionTwoVotes
 
 		return (
 			<div>
-				Would you rather
+				<h1>Would you rather</h1>
 				<div className="wouldYouRather">
 					<div className="option" onClick={ () => this.handleChoose("optionOne")}>
-						<span style={{ color: answeredQuestion === "optionOne" ? "green" : "" }}>
+						<div style={{ color: answeredQuestion === "optionOne" ? "green" : "" }}>
 							{ question.optionOne.text }
-						</span>
+						</div>
+						{ answeredQuestion ? (
+							<div className="statistics">
+								{ `${ Math.round((100 / totalVotes) * optionOneVotes) }% voted for this answer (${ optionOneVotes }/${ totalVotes })` }
+							</div>
+						) : "" }
 					</div>
 					<div className="option" onClick={ () => this.handleChoose("optionTwo")}>
-						<span style={{ color: answeredQuestion === "optionTwo" ? "green" : "" }}>
+						<div style={{ color: answeredQuestion === "optionTwo" ? "green" : "" }}>
 							{ question.optionTwo.text }
-						</span>
+						</div>
+						{ answeredQuestion ? (
+							<div className="statistics">
+								{ `${ Math.round((100 / totalVotes) * optionTwoVotes) }% voted for this answer (${ optionTwoVotes }/${ totalVotes })` }
+							</div>
+						) : "" }
 					</div>
 					<div className="author">
 						<div className="picture">
