519bf8e1380294ae127b4f9dd499f54402220ed7
Erling Tokheim <e@tokheim.no>
2020-07-01 11:17:35 +0200

    Refactor: Simplify question rendering

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import AppBar from '../AppBar/AppBar'
 import toArray from '../../helpers/toArray'
 import Question from '../Question/Question'
+import user from '../../reducers/users'
 
 function mapStateToProps({ questions, authenticatedUser, users }) {
 	return {
@@ -21,48 +22,7 @@ export default connect(mapStateToProps)(class Home extends Component {
 
 	state = {
 		filter: "new",
-		user: this.props.users[this.props.authenticatedUser],
-		newQuestions: [], // Questions the user hasn't answered
-		answeredQuestions: [] // Questions the user already answered
-	}
-
-	// Separates which questions are answered and not in the state's newQuestions and answeredQuestions properties
-	filterQuestions = () => {
-		const { questions } = this.props
-		const { user } = this.state
-
-		// Questions are originally an object containing a lot of objects, so let's convert it to an array for easier handling (in this case)
-		const questionsArray = toArray(questions)
-
-		const answeredQuestions = []
-		const newQuestions = []
-
-		// Filter the questions array
-		questionsArray.map((question) => {
-			// Concatenate all votes to one array
-			const allVotes = question.optionOne.votes.concat(question.optionTwo.votes)
-
-			// Then check if the logged in user is among the votes
-			const voted = allVotes.find((vote) => {
-				if(vote === user.id) {
-					return true
-				} else {
-					return false
-				}
-			})
-
-			// If the logged in user was found among the votes, the question is already answered
-			if(voted) {
-				answeredQuestions.push(question)
-			} else {
-				newQuestions.push(question)
-			}
-		})
-
-		this.setState({
-			answeredQuestions,
-			newQuestions
-		})
+		user: this.props.users[this.props.authenticatedUser]
 	}
 
 	handleChangeFilter = (filter) => {
@@ -71,15 +31,12 @@ export default connect(mapStateToProps)(class Home extends Component {
 		})
 	}
 
-	componentDidMount() {
-		this.filterQuestions();
-	}
-
 	render() {
-		const { answeredQuestions, newQuestions, filter } = this.state
+		const { answeredQuestions, newQuestions, filter, user } = this.state
+		const questions = toArray(this.props.questions)
 
 		// Display the questions corresponding to the filter state
-		const questions = filter === "new" ? newQuestions : answeredQuestions
+		// const questions = filter === "new" ? newQuestions : answeredQuestions
 
 		return (
 			<div>
@@ -93,10 +50,16 @@ export default connect(mapStateToProps)(class Home extends Component {
 						<span>Answered</span>
 					</button>
 					<div className="questions">
-						{/* Render questions */}
-						{ questions.map((question) => (
-							<Question question={ question } key={ question.id } />
-						)) }
+						{
+							questions.filter((question) => filter === "new"
+								? !question.optionOne.votes.concat(question.optionTwo.votes).includes(user.id)
+								: question.optionOne.votes.concat(question.optionTwo.votes).includes(user.id)
+							)
+								.map((question) => (
+									<Question question={ question } key={ question.id } />
+								))
+						}
+						
 					</div>
 				</div>
 				<AppBar />
