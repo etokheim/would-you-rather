8f91a93b63fe0e53fd51ddc5268a68699a98e760
Erling Tokheim <e@tokheim.no>
2020-07-10 14:26:58 +0200

    Refactor: Make app a functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import {
+	BrowserRouter, Switch, Route, Redirect
+} from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import PropTypes from 'prop-types'
 import './App.scss'
@@ -9,49 +11,49 @@ import Home from '../Home/Home'
 import WouldYouRather from '../WouldYouRather/WouldYouRather'
 import NewQuestion from '../NewQuestion/NewQuestion'
 import Leaderboard from '../Leaderboard/Leaderboard'
-import handleInitialData from './../../actions/shared'
+import handleInitialData from '../../actions/shared'
 import NotFound from '../404/NotFound'
 
-function mapStateToProps({ authenticatedUser }) {
-	return {
-		authenticatedUser
-	}
-}
+const mapStateToProps = ({ authenticatedUser }) => ({ authenticatedUser })
 
 const history = createBrowserHistory()
 
-export default connect(mapStateToProps)(class App extends Component {
-	static propTypes = {
-		authenticatedUser: PropTypes.string
-	}
-
-	componentDidMount() {
+export default connect(mapStateToProps)(({ dispatch, authenticatedUser }) => {
+	useEffect(() => {
 		// Get the initial data
-		this.props.dispatch(handleInitialData())
-	}
+		dispatch(handleInitialData())
+	})
 
-	render() {
-		const { authenticatedUser } = this.props
+	return (
+		<BrowserRouter history={history}>
+			History is:
+			{' '}
+			{ JSON.stringify(history) }
+			{
+				// Redirect to login page if the user hasn't logged in, else we can render stuff
+				// that need user data.
+				!authenticatedUser ? (
+					<Redirect to={{
+						pathname: '/login',
+						state: {
+							thingOne: 'its me',
+							intended: '/lalala',
+							redirectFrom: 'state.currentRoute'
+						}
+					}}
+					/>
+				) : (
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/questions/:questionId' component={WouldYouRather} />
+						<Route exact path='/add' component={NewQuestion} />
+						<Route exact path='/leaderboard' component={Leaderboard} />
+					</Switch>
+				)
+			}
 
-		return (
-			<BrowserRouter history={ history }>
-				History is: { JSON.stringify(history) }
-				{
-					// Redirect to login page if the user hasn't logged in, else we can render stuff
-					// that need user data.
-					!authenticatedUser ? <Redirect to="/login" /> : (
-						<Switch>
-							<Route exact path='/' component={ Home } />
-							<Route exact path='/questions/:questionId' component={ WouldYouRather } />
-							<Route exact path='/add' component={ NewQuestion } />
-							<Route exact path='/leaderboard' component={ Leaderboard } />
-						</Switch>
-					)
-				}
-
-				<Route exact path='/login' component={ Login } />
-				<Route exact path='/404' component={ NotFound } />
-			</BrowserRouter>
-		)
-	}
+			<Route exact path='/login' component={Login} />
+			<Route exact path='/404' component={NotFound} />
+		</BrowserRouter>
+	)
 })
