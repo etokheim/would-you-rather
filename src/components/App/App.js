67d9d0e72fc29d0105ca0beb2ef7664571af646b
Erling Tokheim <e@tokheim.no>
2020-07-10 18:20:40 +0200

    Refactor: Lift redirection after login to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import {
 	Switch, Route, Redirect, withRouter
@@ -13,17 +13,47 @@ import Leaderboard from '../Leaderboard/Leaderboard'
 import handleInitialData from '../../actions/shared'
 import NotFound from '../404/NotFound'
 import AppBar from '../AppBar/AppBar'
+import { setAuthenticatedUser } from '../../actions/authenticatedUser'
 
 const mapStateToProps = ({ authenticatedUser }) => ({ authenticatedUser })
 
 export default connect(mapStateToProps)(withRouter(({ dispatch, authenticatedUser, location }) => {
+	// State
+	const [redirect, setRedirect] = useState(false)
+
 	useEffect(() => {
 		// Get the initial data
 		dispatch(handleInitialData())
 	})
 
+	const handleSetAuthenticatedUser = (user) => {
+		dispatch(setAuthenticatedUser(user.id))
+		setRedirect(true)
+	}
+
+	// Redirect if redirect has been set and a user is logged in
+	if (redirect && authenticatedUser) {
+		// Stop redirecting after the user has been redirected
+		// If we stop redirecting immediately, the view don't have time to render, and
+		// will therefor never redirect.
+		if (location.pathname !== '/login') {
+			setRedirect(false)
+		}
+
+		let redirectionUrl = '/'
+		if (location.state && location.state.redirectedFrom !== '/login') {
+			redirectionUrl = location.state.redirectedFrom
+		}
+
+		return (
+			<Redirect to={redirectionUrl} />
+		)
+	}
+
+	// Redirect to login page if the user hasn't logged in
+	// Also append where we are redirecting from to the redirect state. This way we can redirect
+	// back after the user has logged in.
 	if (!authenticatedUser) {
-		// Redirect to login page if the user hasn't logged in
 		if (location.pathname !== '/login') {
 			return (
 				<Redirect
@@ -39,24 +69,28 @@ export default connect(mapStateToProps)(withRouter(({ dispatch, authenticatedUse
 
 		// Display the login page if no user is logged in
 		return (
-			<Route exact path='/login' component={Login} />
+			<Route
+				exact
+				path='/login'
+				render={() => (
+					<Login location={location} handleSetAuthenticatedUser={handleSetAuthenticatedUser} />
+				)}
+			/>
 		)
 	}
 
 	// Else, the user has logged in and we can display components that need user data
 	return (
 		<>
-		<Switch>
-			<Route exact path='/' component={Home} />
-			<Route exact path='/questions/:questionId' component={WouldYouRather} />
-			<Route exact path='/add' component={NewQuestion} />
-			<Route exact path='/leaderboard' component={Leaderboard} />
-
-				{/* <Route exact path='/login' component={Login} /> */}
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/questions/:questionId' component={WouldYouRather} />
+				<Route exact path='/add' component={NewQuestion} />
+				<Route exact path='/leaderboard' component={Leaderboard} />
 
 				{/* If no match, render the 404 page */}
 				<Route component={NotFound} />
-		</Switch>
+			</Switch>
 
 			<AppBar />
 		</>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,45 +1,20 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
 import LoginUserCard from '../LoginUserCard/LoginUserCard'
 import toArray from '../../helpers/toArray'
-import { setAuthenticatedUser } from '../../actions/authenticatedUser'
 
-function mapStateToProps({ users }) {
-	return {
-		users
-	}
-}
+const mapStateToProps = ({ users }) => ({ users })
 
-export default connect(mapStateToProps)(({ users, location, dispatch }) => {
-	// State
-	const [redirect, setRedirect] = useState(false)
-	const [user, setUser] = useState({})
-
-	const handleSetAuthenticatedUser = (user) => {
-		setUser(user)
-		setRedirect(true)
-	}
-
-	// If redirect is set, redirect to it
-	if (redirect) {
-		dispatch(setAuthenticatedUser(user.id))
-
-		return (
-			<Redirect to={location.state.redirectedFrom} />
-		)
-	}
-
-	return (
-		<div>
-			<h1>Login</h1>
-			<h3>Select user</h3>
-			{
-				toArray(users).map((user) => (
-					<LoginUserCard user={user} key={user.id} handleSetAuthenticatedUser={handleSetAuthenticatedUser} />
-				))
-			}
-		</div>
-	)
-})
+export default connect(mapStateToProps)(({
+	users, location, dispatch, handleSetAuthenticatedUser
+}) => (
+	<div>
+		<h1>Login</h1>
+		<h3>Select user</h3>
+		{
+			toArray(users).map((user) => (
+				<LoginUserCard user={user} key={user.id} handleSetAuthenticatedUser={handleSetAuthenticatedUser} />
+			))
+		}
+	</div>
+))
